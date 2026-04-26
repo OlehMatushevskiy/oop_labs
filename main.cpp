0eec30686ad5c50e81d0ed2b69a36065a2f85df3
OlehMatushevskiy <matushevskiy072008@gmail.com>
2026-04-26 18:57:24 +0300

    make lab

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
-#include "AnimalClasses.h"
+#include "TransportClasses.h"
+#include <iostream>
+
+using namespace std;
 
 int main() {
+  Car car("Toyota", 120);
+  Bus bus("Bogdan", 70, 40);
+  Truck truck("MAN", 80, 12);
+  ElectricCar electricCar("Tesla", 85);
 
-  HomeCat *my_pet = new HomeCat("Дворняжка", "Барсік", "Чорно-білий");
-  my_pet->Say();
-  delete my_pet;
+  electricCar.SetSpeed(90, 110);
+
+  Transport *transport;
+
+  cout << "Полiморфiзм у програмi:\n\n";
+
+  transport = &car;
+  transport->ShowInfo();
+  transport->Move();
+  cout << "\n";
+
+  transport = &bus;
+  transport->ShowInfo();
+  transport->Move();
+  cout << "\n";
+
+  transport = &truck;
+  transport->ShowInfo();
+  transport->Move();
+  cout << "\n";
+
+  transport = &electricCar;
+  transport->ShowInfo();
+  transport->Move();
+  cout << "\n";
+
   return 0;
 }
